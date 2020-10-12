fc95425c7b5da12d9961f39a33f965e5980f74b9
Jordan Abu-Sirriya <jordan.abu23@gmail.com>
2020-10-12 17:48:10 -0500

    still working on making letters reveal

--- app.js
+++ app.js
@@ -10,15 +10,51 @@ $(document).ready(function() {
 
     //select one random word from the list
     let randomWord = listOfWords[Math.floor(Math.random()*listOfWords.length)]
-    console.log(randomWord)
     let randomWordSplit = randomWord.split("");
     
     
-    //create a new div for each letter, 
+    //creates a div surrounding the letter and a letter div
     for (i = 0; i<randomWord.length; i++) {
-        $wordOnBoard = $('<div>').addClass('letterOnBoard').text(randomWordSplit[i])
-        $('#guessLetters').append($wordOnBoard)
+        $divAroundLetter = $('<div>').addClass('DivAroundLetter').css('border-style', 'solid');
+        $('#guessLetters').append($divAroundLetter);
+        $letter = $('<div>').addClass('letter').text(randomWordSplit[i]).css('color', 'lightblue');
+        $divAroundLetter.append($letter);
     
     }
 
-});
+  
+ 
+    //main game function
+    const checkCorrectLetter = () => {
+    //player guess
+    let numOfGuesses = 0
+    $('button').on('click', (event) => {
+        let currentGuess = $(event.target).text().toLowerCase()
+        $(event.target).prop('disabled', true);
+        numOfGuesses++
+
+        for (let i=0; i<randomWord.length; i++) {
+            if (currentGuess == randomWord[i]) {
+                $('.letter').eq(i).css('color', 'black').addClass('revealed')
+                    //winning function
+                    if ($('.letter').every('revealed')) {
+                        alert('You win!')
+                    }
+                    
+            }
+        }
+        
+
+        }
+        
+    ) //end of button listener
+} //end of correct letter function
+
+    
+
+
+checkCorrectLetter()
+
+}); //end of jQuery function
+
+
