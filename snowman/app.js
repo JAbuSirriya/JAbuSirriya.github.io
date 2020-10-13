0bda77be1d58440ae5c4b67365d644e6dbecee48
Jordan Abu-Sirriya <jordan.abu23@gmail.com>
2020-10-13 14:06:00 -0500

    winning and losing functions are working again

--- snowman/app.js
+++ snowman/app.js
@@ -7,6 +7,27 @@ $(document).ready(function() {
 //################################################################## Start Game Function  
     
 const startGame = () => {
+
+//################################################################### Winning function 
+// match the correct guess length with the random word 
+    const winningFunction = () => {
+        if (correctGuesses.length == randomWord.length) {
+        console.log('You win!')
+        }
+
+    } //end of winning function
+
+
+    // ########################################################### losing function
+    const losingFunction = () => {
+        if (incorrectGuesses.length === 7) {
+            $('.button').prop('disabled', true);
+            console.log('You lose!')
+        } 
+    } //end of losing function
+
+
+    // Global Variables ##################################################
     let listOfWords = ['elephant', 'headphones', 'dentist']
 
 
@@ -15,6 +36,22 @@ const startGame = () => {
     let randomWordSplit = randomWord.split("");
 
 
+
+    //######################################################### Reset Game function 
+    // const resetGame = () => {
+    // numOfGuesses = [];
+    // $('.button').prop('disabled', false);
+    // generateWord()
+    // checkCorrectLetter()
+
+    // }
+
+    // // // reset game button
+    // $('#reset').on('click', resetGame())
+
+
+
+    //###################################### Generate Word Function
  const generateWord = () => {
     //creates a div surrounding the letter and a letter div
     for (i = 0; i<randomWord.length; i++) {
@@ -26,82 +63,51 @@ const startGame = () => {
 
 } //end of generateWord function
 generateWord()
-//############################################################################
 
+
+//############################################################################ More Global variables
     //array of correct guesses
     let correctGuesses = []
-    //player guess
-    let numOfGuesses = 0
+    let incorrectGuesses = []
+    
+
 
 //#################################################################### check correct letter function
     const checkCorrectLetter = () => {
     $('.button').on('click', (event) => {
         let currentGuess = $(event.target).text().toLowerCase()
         $(event.target).prop('disabled', true);
-        numOfGuesses++
+
     
 
     //correct letters will show up in black 
         for (let i=0; i<randomWord.length; i++) {
-            if (currentGuess == randomWord[i]) {
-            $correctLetter = ($('.letter').eq(i).css('color', 'black').addClass('revealed')) 
+        if (currentGuess == randomWord[i]) {
+        $correctLetter = ($('.letter').eq(i).css('color', 'black').addClass('revealed'))
+        //sends correct letters to correctGuesses array
+        correctGuesses.push($correctLetter)
+        } else if (currentGuess != randomWord[i]) {
+            const num =+ 1
+            incorrectGuesses.push(num)
+            break
+        }
 
-            //sends correct letters to correctGuesses array
-            correctGuesses.push($correctLetter)
-            }
         }
         
-    })
-    console.log(randomWord.length)
-    console.log(correctGuesses.length)
+        winningFunction()
+        losingFunction()
+    } 
+    )
+
+
+
  } //end of checkCorrectLetter
 checkCorrectLetter()
 
-//################################################################### Winning function 
-
-    // winning function, match the correct guess length with the random word 
-        const winningFunction = () => {
-            if (correctGuesses.length == randomWord.length) {
-            console.log(randomWord.length)
-            console.log(correctGuesses.length)
-            console.log('You win!')
-            }
-    
-        } //end of winning function
-winningFunction()
-    
-
-//############################################################### Losing function
-    
-    // losing function
-    const losingFunction = () => {
-        if (numOfGuesses === 7) {
-            $('.button').prop('disabled', true);
-            console.log('You lose!')
-        }
-        
-    } //end of losing function
-losingFunction()
 
 
 
-  //######################################################### Reset Game function  
-    
-    //reset game function
-    // const resetGame = () => {
-    //     numOfGuesses = [];
-    //     $('.button').prop('disabled', false);
-    //     generateWord()
-    //     checkCorrectLetter()
-
-    //     }
-    
-    // // reset game button
-    // $('#reset').on('click', resetGame())
-
-
-
-} //main game function
+} // end of main game function
 
 
 startGame()
